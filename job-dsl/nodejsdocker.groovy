60b92a62d1d6e4a945c585613bb2f2d6f0a3ad0f
dogershub <123512438+dogershub@users.noreply.github.com>
2024-07-05 16:10:34 -0400

    Update nodejsdocker.groovy-56

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('dogerthat/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('docker-id')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
