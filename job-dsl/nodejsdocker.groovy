2c02b033f2cd337ecd39d868904192c3f8206034
dogershub <123512438+dogershub@users.noreply.github.com>
2024-07-05 14:55:51 -0400

    Update nodejsdocker.groovy = REAL URL

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/dogershub/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/dogershub/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
